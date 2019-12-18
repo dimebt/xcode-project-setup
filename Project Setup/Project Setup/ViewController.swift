5f7ba124df3d3992d88293c0eb1589282b503b14
Dimitar Stefanovski <dimebt_2005@hotmail.com>
2019-12-18 07:45:09 +0100

    Initial commit

---
+++ Project Setup/Project Setup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project Setup
+//
+//  Created by Dimitar Stefanovski on 12/18/19.
+//  Copyright © 2019 Dimitar Stefanovski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
